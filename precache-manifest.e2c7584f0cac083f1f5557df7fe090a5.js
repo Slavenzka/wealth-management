f34601c1bb242784df2183392350de11efe3d5f1
Ivan <i.romanov@protonmail.com>
2020-08-25 18:41:56 +0300

    Updates

---
+++ precache-manifest.e2c7584f0cac083f1f5557df7fe090a5.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0d42481949f10098f59c8372d2068e8",
+    "url": "/wealth-management/index.html"
+  },
+  {
+    "revision": "c3995ad5f4c70a191a01",
+    "url": "/wealth-management/static/css/2.ed64c74d.chunk.css"
+  },
+  {
+    "revision": "886637d0524d2f398fb1",
+    "url": "/wealth-management/static/css/main.414b47f2.chunk.css"
+  },
+  {
+    "revision": "c3995ad5f4c70a191a01",
+    "url": "/wealth-management/static/js/2.12045a43.chunk.js"
+  },
+  {
+    "revision": "2409169cc9d488610c63e21c6ccf8ee2",
+    "url": "/wealth-management/static/js/2.12045a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "886637d0524d2f398fb1",
+    "url": "/wealth-management/static/js/main.d1354137.chunk.js"
+  },
+  {
+    "revision": "2e4a82ac748f94c1c2dc",
+    "url": "/wealth-management/static/js/runtime-main.7f5282ef.js"
+  },
+  {
+    "revision": "231495e51444c162bed5df57ee968c8e",
+    "url": "/wealth-management/static/media/FuturaPT-Book.231495e5.woff2"
+  },
+  {
+    "revision": "54a123e5410e5dadf16d8b368fa0ab54",
+    "url": "/wealth-management/static/media/FuturaPT-Book.54a123e5.woff"
+  },
+  {
+    "revision": "081741b541afdaa972cb5f76ac6d3a5e",
+    "url": "/wealth-management/static/media/FuturaPT-ExtraBold.081741b5.woff2"
+  },
+  {
+    "revision": "11ba3ac6bb29be79323bda60fb72e9fb",
+    "url": "/wealth-management/static/media/FuturaPT-ExtraBold.11ba3ac6.woff"
+  },
+  {
+    "revision": "8fa18513e8b92c123707ed12dc54b9da",
+    "url": "/wealth-management/static/media/FuturaPT-Medium.8fa18513.woff"
+  },
+  {
+    "revision": "9819eaf042d4ad45186a5b49dfae95b4",
+    "url": "/wealth-management/static/media/FuturaPT-Medium.9819eaf0.woff2"
+  },
+  {
+    "revision": "04b82b15da4bf7682d0944a396365143",
+    "url": "/wealth-management/static/media/FuturaPTBold.04b82b15.woff2"
+  },
+  {
+    "revision": "b1755e863dafc58f2dd34f8ba8f7479b",
+    "url": "/wealth-management/static/media/FuturaPTBold.b1755e86.woff"
+  },
+  {
+    "revision": "1726f096cc123b7eaa1351f200227471",
+    "url": "/wealth-management/static/media/FuturaPTDemi.1726f096.woff"
+  },
+  {
+    "revision": "29fa49c1fce0e2d03503cfee4a50e9f9",
+    "url": "/wealth-management/static/media/FuturaPTDemi.29fa49c1.woff2"
+  },
+  {
+    "revision": "2b26e1de63567ecb5169be73e9f9b9db",
+    "url": "/wealth-management/static/media/GetConsultBanner__img.2b26e1de.jpg"
+  },
+  {
+    "revision": "419898ec6026522cef6e44a9d1b8d8d7",
+    "url": "/wealth-management/static/media/PortfolioBanner__diagram.419898ec.png"
+  },
+  {
+    "revision": "1568e45d38cd858636c33bcdc6f6e027",
+    "url": "/wealth-management/static/media/PresentationBanner__img.1568e45d.png"
+  },
+  {
+    "revision": "9547568bcc491a89024d4d3acc0984d3",
+    "url": "/wealth-management/static/media/PromoBanner__img.9547568b.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/wealth-management/precache-manifest.e2c7584f0cac083f1f5557df7fe090a5.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wealth-management/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
