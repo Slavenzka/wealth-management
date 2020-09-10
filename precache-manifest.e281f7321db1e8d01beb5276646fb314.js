5faf11ced31f1edddcedfabbfca5728d96448c4c
Ivan <i.romanov@protonmail.com>
2020-09-10 20:55:34 +0300

    Updates

---
+++ precache-manifest.e281f7321db1e8d01beb5276646fb314.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b38f2ca9670b267ed8173e27837bc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ed37ec71e336b823580",
+    "url": "/static/css/2.6ae3693d.chunk.css"
+  },
+  {
+    "revision": "3bb24e9c862eaa305e1e",
+    "url": "/static/css/main.b36f4c42.chunk.css"
+  },
+  {
+    "revision": "3ed37ec71e336b823580",
+    "url": "/static/js/2.126ebaa0.chunk.js"
+  },
+  {
+    "revision": "5b099948b207b77f6fa04ff33fa98702",
+    "url": "/static/js/2.126ebaa0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bb24e9c862eaa305e1e",
+    "url": "/static/js/main.54c6b0bf.chunk.js"
+  },
+  {
+    "revision": "c093561b65b02841917d",
+    "url": "/static/js/runtime-main.54a11f56.js"
+  },
+  {
+    "revision": "231495e51444c162bed5df57ee968c8e",
+    "url": "/static/media/FuturaPT-Book.231495e5.woff2"
+  },
+  {
+    "revision": "54a123e5410e5dadf16d8b368fa0ab54",
+    "url": "/static/media/FuturaPT-Book.54a123e5.woff"
+  },
+  {
+    "revision": "081741b541afdaa972cb5f76ac6d3a5e",
+    "url": "/static/media/FuturaPT-ExtraBold.081741b5.woff2"
+  },
+  {
+    "revision": "11ba3ac6bb29be79323bda60fb72e9fb",
+    "url": "/static/media/FuturaPT-ExtraBold.11ba3ac6.woff"
+  },
+  {
+    "revision": "8fa18513e8b92c123707ed12dc54b9da",
+    "url": "/static/media/FuturaPT-Medium.8fa18513.woff"
+  },
+  {
+    "revision": "9819eaf042d4ad45186a5b49dfae95b4",
+    "url": "/static/media/FuturaPT-Medium.9819eaf0.woff2"
+  },
+  {
+    "revision": "04b82b15da4bf7682d0944a396365143",
+    "url": "/static/media/FuturaPTBold.04b82b15.woff2"
+  },
+  {
+    "revision": "b1755e863dafc58f2dd34f8ba8f7479b",
+    "url": "/static/media/FuturaPTBold.b1755e86.woff"
+  },
+  {
+    "revision": "1726f096cc123b7eaa1351f200227471",
+    "url": "/static/media/FuturaPTDemi.1726f096.woff"
+  },
+  {
+    "revision": "29fa49c1fce0e2d03503cfee4a50e9f9",
+    "url": "/static/media/FuturaPTDemi.29fa49c1.woff2"
+  },
+  {
+    "revision": "2b26e1de63567ecb5169be73e9f9b9db",
+    "url": "/static/media/GetConsultBanner__img.2b26e1de.jpg"
+  },
+  {
+    "revision": "fd0766a93c533d54c51466fcc703e6f0",
+    "url": "/static/media/PortfolioBanner__diagram.fd0766a9.png"
+  },
+  {
+    "revision": "edf798771ce9b5ea3d9d63ca24c1ee73",
+    "url": "/static/media/PresentationBanner__img.edf79877.png"
+  },
+  {
+    "revision": "2bfc1e530cf55fad14991cbb5d9a45e0",
+    "url": "/static/media/PromoBanner__img.2bfc1e53.png"
+  },
+  {
+    "revision": "a8556ad6997dae2d4d8e65cf00f9f781",
+    "url": "/static/media/slider__item--1.a8556ad6.png"
+  },
+  {
+    "revision": "f65f1533ce4ed62e32924a657f9e1546",
+    "url": "/static/media/slider__item--2.f65f1533.png"
+  },
+  {
+    "revision": "ea92dfab08c792970888bd75a341f14b",
+    "url": "/static/media/slider__item--3.ea92dfab.png"
+  },
+  {
+    "revision": "78c171d47615e3de82e7e7d101e353d9",
+    "url": "/static/media/slider__item--4.78c171d4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/wealth-management/precache-manifest.0dd7c1cd94c2470deb6b62f44979048e.js"
+  "/precache-manifest.e281f7321db1e8d01beb5276646fb314.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wealth-management/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
